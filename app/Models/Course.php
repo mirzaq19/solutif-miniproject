23bd0867751a2b10c7131e0e7b1fe43f533a6ce3
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-08-29 16:32:10 +0700

    feat: add course (table, model, seeder)

---
+++ app/Models/Course.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+    use UUID;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'credit',
+    ];
+}
