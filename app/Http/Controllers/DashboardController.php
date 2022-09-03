51b6274f5240833a0a68e46d2cdd0d88287c6d58
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-09-03 08:10:46 +0700

    feat: add card summary to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $numberOfStudent = Student::all()->count();
+        $numberOfCourse = Course::all()->count();
+
+        return view('dashboard.index', compact('numberOfStudent', 'numberOfCourse'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,47 @@
 @extends('layout')
 @section('title', 'Dashboard')
 @section('content')
-    <div class="container min-h-screen lg:h-min mx-auto px-4 py-10 flex flex-col md:flex-row md:justify-between gap-y-4 md:gap-x-4">
+    <div class="container min-h-screen lg:h-min mx-auto px-4 py-10">
         <h1 class="font-bold text-2xl">Dashboard</h1>
+
+        <div class="mt-8 flex flex-col gap-y-4">
+            <div class="rounded-lg border">
+                <div class="border-b flex p-4 bg-gray-50">
+                    <h3 class="font-semibold text-base md:text-lg"><i class="fas fa-user text-indigo-500"></i> Mahasiswa</h3>
+                </div>
+                <div class="p-4">
+                    <div class="flex items-center justify-between mb-5">
+                        <div class="flex flex-col">
+                            <h3 class="font-semibold text-base md:text-lg">Total Mahasiswa</h3>
+                            <p class="text-gray-500 text-xs md:text-sm">Total mahasiswa yang terdaftar</p>
+                        </div>
+                        <div class="flex flex-col">
+                            <h3 class="font-semibold text-base md:text-lg">{{ $numberOfStudent }}</h3>
+                            <p class="text-gray-500 text-xs md:text-sm">Orang</p>
+                        </div>
+                    </div>
+                    <a href="#" class="font-semibold text-sm md:text-base hover:text-indigo-700 transition-colors duration-300">Lihat detail</a>
+                </div>
+            </div>
+
+            <div class="rounded-lg border">
+                <div class="border-b flex p-4 bg-gray-50">
+                    <h3 class="font-semibold text-base md:text-lg"><i class="fas fa-book text-indigo-500"></i> Mata Kuliah</h3>
+                </div>
+                <div class="p-4">
+                    <div class="flex items-center justify-between mb-5">
+                        <div class="flex flex-col">
+                            <h3 class="font-semibold text-base md:text-lg">Total Mata Kuliah</h3>
+                            <p class="text-gray-500 text-xs md:text-sm ">Total mata kuliah yang terdaftar</p>
+                        </div>
+                        <div class="flex flex-col">
+                            <h3 class="font-semibold text-base md:text-lg">{{ $numberOfCourse }}</h3>
+                            <p class="text-gray-500 text-xs  md:text-sm">Buah</p>
+                        </div>
+                    </div>
+                    <a href="#" class="font-semibold text-sm md:text-base hover:text-indigo-700 transition-colors duration-300">Lihat detail</a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
